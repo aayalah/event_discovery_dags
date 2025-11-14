df4834d77d1dc5516d6e973d4b6ea43c19ef38c3
aayalah <amayalah@alumni.stanford.edu>
2025-11-13 20:19:43 -0500

    added kafka producer task

--- dags/fetch_ticket_master.py
+++ dags/fetch_ticket_master.py
@@ -2,11 +2,15 @@ from __future__ import annotations
 
 import os
 import requests
+import json
 from datetime import datetime
 from airflow.decorators import dag, task
+from kafka import KafkaProducer
 
 
 TICKETMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
+KAFKA_BROKERS = os.getenv("KAFKA_BROKERS")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
 BASE_PATH = "/usr/local/airflow/data"   # this exists in your container
 FILENAME = "ticketmaster_events.json"
 
@@ -21,7 +25,7 @@ def fetch_ticketmaster_pipeline():
     """Fetch events from Ticketmaster API and log them."""
 
     @task
-    def fetch_events(city: str = "San Francisco", size: int = 10):
+    def fetch_events(city: str = "San Francisco", size: int = 100) -> list[dict]:
         if not TICKETMASTER_API_KEY:
             raise ValueError("Missing TICKETMASTER_API_KEY environment variable")
 
@@ -39,10 +43,10 @@ def fetch_ticketmaster_pipeline():
             print(f" - {e['name']}")
 
         # return to next task or XCom
-        return [{"id": e["id"], "name": e["name"]} for e in events]
+        return [e for e in events]
 
     @task
-    def save_events_to_file(events: list[dict]):
+    def save_events_to_file(events: list[dict]) -> str:
         """Save fetched events to a temporary file."""
         import json
         os.makedirs(BASE_PATH, exist_ok=True)
@@ -54,10 +58,24 @@ def fetch_ticketmaster_pipeline():
         print("Directory listing:")
         print(os.listdir(BASE_PATH))
         return full_path
+    
+    @task
+    def publish_to_kafka(event: dict):
+        if not KAFKA_BROKERS:
+            raise ValueError("Missing KAFKA_BROKERS environment variable")
+        if not KAFKA_TOPIC:
+            raise ValueError("Missing KAFKA_TOPIC environment variable")
+        
+        brokers = [b.strip() for b in KAFKA_BROKERS.split(",") if b.strip()]
+        producer = KafkaProducer(bootstrap_servers=brokers) # Replace with your Kafka broker address
+        producer.send(KAFKA_TOPIC, json.dumps(event).encode('utf-8'))
+        producer.flush()
+        producer.close()
 
     # Define task order
     events = fetch_events()
     save_events_to_file(events)
+    publish_to_kafka.expand(event=events)
 
 
 fetch_ticketmaster_pipeline()
